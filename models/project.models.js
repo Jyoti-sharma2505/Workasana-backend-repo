6f6bdc594a23a00b94f8ee0b2703c198bec053b8
Jyoti-Sharma <jyo.sharma2505@gmail.com>
2025-11-28 11:59:02 +0530

    fixed : project and task

--- models/project.models.js
+++ models/project.models.js
@@ -4,6 +4,10 @@ const mongoose = require('mongoose');
 const projectSchema = new mongoose.Schema({
  name: { type: String, required: true, unique: true }, // Project names must be unique
  description: { type: String }, // Optional field for project details
+ status : {
+    type: String,
+    enum :['To Do', 'In Progress', 'Completed', 'Blocked']
+ },
  createdAt: { type: Date, default: Date.now }
 });
 module.exports = mongoose.model('Project', projectSchema);
